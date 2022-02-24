8d7cb9928f8333e8334bb6a96c9b40ee7ac99ed2
Janishnehyan03 <nehyanjanish@gmail.com>
2022-02-24 10:23:45 +0530

    dashboard theme changed

--- client/src/pages/Admin/Dashboard.jsx
+++ client/src/pages/Admin/Dashboard.jsx
@@ -3,110 +3,121 @@ import { Link } from "react-router-dom";
 
 function Dashboard() {
   return (
-    //  overview of the project
-    <div className="flex flex-col h-screen">
-      <div className="ring-1 ring-black ring-opacity-5 bg-white divide-y divide-dashed py-10 mb-4">
-        <h1 className="text-center text-3xl font-bold text-blue-900">
-          VAHDA Dashboard
-        </h1>
-      </div>
-      {/* 3 card for create post , manage authors, categories */}
-      <div className="flex flex-wrap justify-center">
-        <div className="w-full max-w-sm mx-4 my-2">
-          <div className="flex flex-col break-words bg-white border-2 rounded shadow-md">
-            <div className="flex justify-center border-b-2 p-3">
+    <>
+      <section className="bg-white dark:bg-gray-900">
+        <div className="container px-6 py-10 mx-auto">
+          <h1 className="text-3xl font-semibold text-gray-800 capitalize lg:text-4xl dark:text-white">
+            VAHDA <br /> Admin{" "}
+            <span className="underline decoration-blue-500">Dashboard</span>
+          </h1>
+          <p className="mt-4 text-gray-500 xl:mt-6 dark:text-gray-300">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nostrum
+            quam voluptatibus
+          </p>
+          <div className="grid grid-cols-1 gap-8 mt-8 xl:mt-12 xl:gap-12 md:grid-cols-2 xl:grid-cols-3">
+            <div className="p-8 space-y-3 border-2 border-blue-400 dark:border-blue-300 rounded-xl">
               <Link
                 to={"/create-post"}
-                className="font-bold text-xl mb-0 bg-gray-900 py-2 px-4 text-white"
+                className="text-2xl bg-blue-500 px-4 py-2 rounded-lg font-semibold text-gray-700 capitalize dark:text-white"
               >
-                Create Post
+                Create New Post
               </Link>
-            </div>
-            <div className="flex-1 p-6">
+              <p className="text-gray-500 dark:text-gray-300">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Provident ab nulla quod dignissimos vel non corrupti doloribus
+                voluptatum eveniet
+              </p>
               <Link
-                to={"/all-posts/"}
-                className="text-indigo-500 inline-flex items-center mt-3"
+                to={"/all-posts"}
+                className="inline-flex p-2 text-blue-500 capitalize transition-colors duration-200 transform bg-blue-100 rounded-full dark:bg-blue-500 dark:text-white hover:bg-white hover:text-blue-600 dark:hover:text-blue-500"
               >
-                view posts
+                view all
                 <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="w-6 h-6"
                   fill="none"
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  className="w-4 h-4 ml-2"
                   viewBox="0 0 24 24"
+                  stroke="currentColor"
                 >
-                  <path d="M5 12h14M12 5l7 7-7 7" />
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M13 9l3 3m0 0l-3 3m3-3H8m13 0a9 9 0 11-18 0 9 9 0 0118 0z"
+                  />
                 </svg>
               </Link>
             </div>
-          </div>
-        </div>
-        <div className="w-full max-w-sm mx-4 my-2">
-          <div className="flex flex-col break-words bg-white border-2 rounded shadow-md">
-            <div className="flex justify-center border-b-2 p-3">
-              <Link
-                to={"/create-category"}
-                className="font-bold text-xl mb-0 bg-gray-800 py-2 px-4 text-white"
-              >
-                Create Category
-              </Link>
-            </div>
-            <div className="flex-1 p-6">
-            <Link
-                to={"/create-category/"}
-                className="text-indigo-500 inline-flex items-center mt-3"
-              >
-                view categories
-                <svg
-                  fill="none"
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  className="w-4 h-4 ml-2"
-                  viewBox="0 0 24 24"
-                >
-                  <path d="M5 12h14M12 5l7 7-7 7" />
-                </svg>
-              </Link>
-            </div>
-          </div>
-        </div>
-        <div className="w-full max-w-sm mx-4 my-2">
-          <div className="flex flex-col break-words bg-white border-2 rounded shadow-md">
-            <div className="flex justify-center border-b-2 p-3">
+            <div className="p-8 space-y-3 border-2 border-blue-400 dark:border-blue-300 rounded-xl">
               <Link
                 to={"/create-author"}
-                className="font-bold text-xl mb-0 bg-gray-800 py-2 px-4 text-white"
+                className="text-2xl bg-blue-500 px-4 py-2 rounded-lg font-semibold text-gray-700 capitalize dark:text-white"
               >
-                Create Author
+                Create New Author
+              </Link>
+              <p className="text-gray-500 dark:text-gray-300">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Provident ab nulla quod dignissimos vel non corrupti doloribus
+                voluptatum eveniet
+              </p>
+              <Link
+                to={"/all-authors"}
+                className="inline-flex p-2 text-blue-500 capitalize transition-colors duration-200 transform bg-blue-100 rounded-full dark:bg-blue-500 dark:text-white hover:bg-white hover:text-blue-600 dark:hover:text-blue-500"
+              >
+                view all
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="w-6 h-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M13 9l3 3m0 0l-3 3m3-3H8m13 0a9 9 0 11-18 0 9 9 0 0118 0z"
+                  />
+                </svg>
               </Link>
             </div>
-            <div className="flex-1 p-6">
+            <div className="p-8 space-y-3 border-2 border-blue-400 dark:border-blue-300 rounded-xl">
               <Link
-                to={"/all-authors/"}
-                className="text-indigo-500 inline-flex items-center mt-3"
+                to={"/create-category"}
+                className="text-2xl bg-blue-500 px-4 py-2 rounded-lg font-semibold text-gray-700 capitalize dark:text-white"
               >
-                view authors 
+                Create New Category
+              </Link>
+              <p className="text-gray-500 dark:text-gray-300">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Provident ab nulla quod dignissimos vel non corrupti doloribus
+                voluptatum eveniet
+              </p>
+              <Link
+                to={"/create-category"}
+                className="inline-flex p-2 text-blue-500 capitalize transition-colors duration-200 transform bg-blue-100 rounded-full dark:bg-blue-500 dark:text-white hover:bg-white hover:text-blue-600 dark:hover:text-blue-500"
+              >
+                view all
                 <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="w-6 h-6"
                   fill="none"
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  className="w-4 h-4 ml-2"
                   viewBox="0 0 24 24"
+                  stroke="currentColor"
                 >
-                  <path d="M5 12h14M12 5l7 7-7 7" />
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M13 9l3 3m0 0l-3 3m3-3H8m13 0a9 9 0 11-18 0 9 9 0 0118 0z"
+                  />
                 </svg>
               </Link>
             </div>
           </div>
         </div>
-      </div>
-    </div>
+      </section>
+    </>
   );
 }
 
